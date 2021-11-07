e822877471105c1a7ca7d84eb4235cdf31882ea1
Mikhail Cruz Andrade <jailandrade@gmail.com>
2021-11-06 19:18:57 -0600

    first exercise

---
+++ src/tacos.js
@@ -0,0 +1,30 @@
+export const tacos = [
+    "taco de pastor",
+    "taco de carnitas",
+    "taco de camaron",
+    "taco de longaniza",
+    "taco de bistec"
+]
+
+export const tacos_precios = [
+    {
+        "taco": "de pastor",
+        "precio": "20"
+    },
+    {
+        "taco": "de carnitas",
+        "precio": "30"
+    },
+    {
+        "taco": "de camaron",
+        "precio": "40"
+    },
+    {
+        "taco": "de longaniza",
+        "precio": "15"
+    },
+    {
+        "taco": "de bistec",
+        "precio": "20"
+    }
+]
