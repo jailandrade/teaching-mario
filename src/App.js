d9c6969fd38215a223ef70c393f29d7f012dd372
Mikhail Cruz Andrade <jailandrade@gmail.com>
2021-11-06 19:52:07 -0600

    adds comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import { tacos, tacos_precios } from './tacos.js';
 
+// Aqui estamos extrayendo el nombre de la propiedad
+// en el que estamos pasando los valores
 const Taco = ({items}) => {
+  // Podemos utilizr JSX para iterar sobre una lista
+  // y guardarla como un conjunto de elementos
   const listItems = items.map((item) => 
     <li>{item.taco} - {item.precio}</li>
   );
 
   return (
+    // Aqui estamos insertando la lista en el momento 
+    // de regresarla
     <ul>{listItems}</ul>
   )
 }
