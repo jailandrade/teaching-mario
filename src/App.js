6fbcf99868bcfb7abe6ae933db5dab99c32c3287
Mikhail Cruz Andrade <jailandrade@gmail.com>
2021-11-06 19:49:45 -0600

    Separates the component Taco from the main function.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import { tacos, tacos_precios } from './tacos.js';
 
+const Taco = ({items}) => {
+  const listItems = items.map((item) => 
+    <li>{item.taco} - {item.precio}</li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <ul>
-        {tacos.map((taco) => 
-          <li>{taco}</li>
-        )}
-      </ul>
+      <Taco items={tacos_precios} />
     </div>
   );
 }
